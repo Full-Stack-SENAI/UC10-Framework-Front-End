8bd5f48cd14e1b5d33889724d252b53f8c1b8f20
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-10 22:30:14 -0300

    Ajuste no app module e renomeação de pasta

--- aula42-181125-Proj-Angular2/agenda-contatos/src/app/app.module.ts
+++ aula42-181125-Proj-Angular2/agenda-contatos/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ContatosComponent } from './contatos/contatos.component';
 
@@ -12,7 +13,7 @@ import { ContatosComponent } from './contatos/contatos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
