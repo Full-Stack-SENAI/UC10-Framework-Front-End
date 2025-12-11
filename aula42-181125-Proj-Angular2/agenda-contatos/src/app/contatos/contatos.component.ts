b63dbab3523b3029e515d3577d877f477f2f613f
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-11 15:20:13 -0300

    Finalizando aula 42 - Entendendo algumas funções do Angular

--- aula42-181125-Proj-Angular2/agenda-contatos/src/app/contatos/contatos.component.ts
+++ aula42-181125-Proj-Angular2/agenda-contatos/src/app/contatos/contatos.component.ts
@@ -1,10 +1,38 @@
+import { trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterTestingHarness } from '@angular/router/testing';
 
+interface Contato{
+  nome: string;
+  telefone: string;
+}
 @Component({
   selector: 'app-contatos',
   templateUrl: './contatos.component.html',
   styleUrls: ['./contatos.component.css']
 })
 export class ContatosComponent {
+  novoNome: string = '';
+  novoTelefone: string = '';
 
+  contatos: Contato[] = [];
+
+  adicionarContato(){
+    const nome = this.novoNome.trim();
+    const telefone = this.novoTelefone.trim();
+
+    if(nome === '' || telefone === '') return;
+    this.contatos.push({nome, telefone});
+    this.novoNome = '';
+    this.novoTelefone = '';
+
+  }
+
+  removerContato(index: number){
+    this.contatos.splice(index, 1);
+  }
+
+
+
+  
 }
